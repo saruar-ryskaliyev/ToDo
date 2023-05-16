2f7ec203d8ec63729b1abd3f895b25758120ca0a
saruar-ryskaliyev <108226977+saruar-ryskaliyev@users.noreply.github.com>
2023-05-17 00:21:05 +0600

    Add files via upload

--- ToDo/Controller/CustomCell.swift
+++ ToDo/Controller/CustomCell.swift
@@ -2,46 +2,34 @@
 //  CustomCell.swift
 //  ToDo
 //
-//  Created by Saruar on 11.05.2023.
+//  Created by Saruar on 17.05.2023.
 //
 
+import Foundation
 import UIKit
 
+
 class CustomCell: UITableViewCell{
-    static let identifier = "CustomCell"
+    
+    
+    static let identifier = "CustomTableViewCell"
+    
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-
-        self.contentView.layer.cornerRadius = 15
-        self.contentView.clipsToBounds = true
-        self.contentView.layer.borderWidth = 1
         
-        self.contentView.backgroundColor = .systemGray
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+       
+        
+        backgroundColor = UIColor.white
+        contentView.layer.borderColor = UIColor.black.cgColor
+        contentView.layer.borderWidth = 1
+        contentView.layer.cornerRadius = 8
+        clipsToBounds = true
     }
     
-    
-    override var frame: CGRect {
-        get {
-            return super.frame
-        }
-        set (newFrame) {
-            var frame = newFrame
-            let newWidth = frame.width * 0.95 // get 80% width here
-            let space = (frame.width - newWidth) / 2
-            frame.size.width = newWidth
-            frame.origin.x += space
-
-            super.frame = frame
-
-        }
-    }
-    
-
 
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
 }
--- ToDo/Controller/ViewController.swift
+++ ToDo/Controller/ViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    let customCell = CustomCell()
     let CoreDataModel = CoreDataManager()
     let cellSpacingHeight: CGFloat = 5
     
@@ -84,39 +83,71 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     }
     
- 
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        CoreDataModel.getAllItems()
+        return CoreDataModel.tasks.count
+    }
+    
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return cellSpacingHeight
+    }
     
     func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
         
         return .delete
+    
     }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.clear
+        return headerView
+    }
+    
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete{
-            tableView.beginUpdates()
             
-            tableView.deleteRows(at: [indexPath], with: .fade)
-            
-            let task = CoreDataModel.tasks[indexPath.row]
-            
-            
+            let task = CoreDataModel.tasks[indexPath.section]
             CoreDataModel.deleteItem(item: task)
+            CoreDataModel.getAllItems()
+            
+            tableView.beginUpdates()
+        
+            tableView.deleteSections(IndexSet(integer: indexPath.section), with: .fade)
+            
             
             tableView.endUpdates()
         }
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return CoreDataModel.tasks.count
-    }
+
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let task = CoreDataModel.tasks[indexPath.row]
+        let task = CoreDataModel.tasks[indexPath.section]
         
         let cell = tableView.dequeueReusableCell(withIdentifier: CustomCell.identifier, for: indexPath)
         
         cell.textLabel?.text = task.tasks
+        
+        
+//        cell.backgroundColor = UIColor.white
+//        cell.layer.borderColor = UIColor.black.cgColor
+//        cell.layer.borderWidth = 1
+//        cell.layer.cornerRadius = 8
+//        cell.clipsToBounds = true
+       
+  
         
         let imageView = UIImageView(frame: CGRect(x: 15, y: 0, width: 25, height: 25))
         imageView.backgroundColor = .clear
@@ -140,13 +171,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
-
+  
+    
 
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let cell = tableView.cellForRow(at: indexPath)
         
-        let task = CoreDataModel.tasks[indexPath.row]
+        let task = CoreDataModel.tasks[indexPath.section]
         
         
         if task.isSelected == true{
@@ -180,6 +212,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
 
-
 }
+
 
